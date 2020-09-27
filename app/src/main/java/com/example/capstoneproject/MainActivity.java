b84ad5283b5fbc5a494cda33eb389626bcd97ff8
Hafizh Zahran <zahran.hafizh@yahoo.co.id>
2020-09-28 04:15:07 +0700

    Fragment dan menu

--- app/src/main/java/com/example/capstoneproject/MainActivity.java
+++ app/src/main/java/com/example/capstoneproject/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.capstoneproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
